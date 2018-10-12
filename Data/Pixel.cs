b0d9521cf18ed573c2cbcba68f612331abce259f
spv <s_ptr@rambler.ru>
2018-10-12 13:12:48 +0500

    Добавлен контроль значений каналов для точек

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -6,10 +6,50 @@ using System.Text;
 namespace MyPhotoshop
 {
     public class Pixel
-    {
-        public double R;
-        public double G;
-        public double B;
+    {   
+        public double Check(double value)
+        {
+            if (value < 0 || value > 1) throw new ArgumentException();
+                return value;
+        }
+
+        public static double Trim(double value)
+        {
+            if (value < 0) return 0;
+            if (value > 1) return 1;
+            return value;
+        }
+
+        double r;
+        public double R
+        {
+            get { return r; }
+            set
+            {
+                r = Check(value);
+            }
+        }
+
+        double g;
+        public double G
+        {
+            get { return g; }
+            set
+            {
+                g = Check(value);
+            }
+        }
+
+        double b;
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                b = Check(value);
+            }
+        }
+        
        
     }
 }
